4d99b83a01bf42ca4d0dd45df2c5c1333277cc7b
Pablo Brajus <psbrajus@gmail.com>
2025-08-24 13:12:43 -0300

    pruebas

--- backend-node/server.js
+++ backend-node/server.js
@@ -5,6 +5,8 @@ import pool from "./db.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+
 // 🔹 Middleware
 app.use(cors()); // Habilita CORS para todos los dominios (puedes restringirlo si quieres)
 app.use(express.json()); // Parseo de JSON en requests
